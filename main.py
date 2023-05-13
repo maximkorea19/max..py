ac382cbe728facc26ba4722800f11ef216e210a3
maximkorea19 <133477132+maximkorea19@users.noreply.github.com>
2023-05-13 14:33:22 +0000

    maximkorea19

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print("hello")
+# """ "Hello world" """
+# ''' 'Hello worls' '''
+
+#여러줄의 문자열을 출력하고싶을때\n
+#mutilline''' or """
+#Life is too short
+#You need python
+#''' or """
+#print(mutilline)
+
+#문자열 길이 구하기(len)
+# a = 'You need pyrhon'
+# print(len(a))
+
+#문자열 인덱싱(indexing)=가르키다,슬라이싱(Slicing)=잘라낸다
+# a = 'Life is too short, You need python'
+#      0         1         2         3
+#      0123456789012345678901234567890123
+#a = 'Life is too short, you need python'
+# (예)print(a[3])=e
+#print(a[-3])마이너스는 뒤에서부터
+
+
