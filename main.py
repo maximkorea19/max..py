73afc08894f4ec7cc2345a16c484d2361bb74ec4
maximkorea19 <133477132+maximkorea19@users.noreply.github.com>
2023-05-14 02:14:03 +0000

    hello

--- main.py
+++ main.py
@@ -13,11 +13,23 @@
 # print(len(a))
 
 #문자열 인덱싱(indexing)=가르키다,슬라이싱(Slicing)=잘라낸다
+#문자열 인덱싱(indexing)
 # a = 'Life is too short, You need python'
 #      0         1         2         3
 #      0123456789012345678901234567890123
 #a = 'Life is too short, you need python'
 # (예)print(a[3])=e
 #print(a[-3])마이너스는 뒤에서부터
+############## 인덱싱은 원하는 곳을 가르켜 골라온다
+#문자열 슬라이싱(slicing)
+# a = 'Life is too short, You need python'
+# b = a[0] + a[1] + a[2] + a[3]
+# print(a)='Life'
+### 슬라이싱 기법을 간단하게
+# a = 'Life is too short, You need
+##############슬라이싱은 원하는 
 
 
+a = 'Life is too short, You need python'
+b= a[0]+a[10]+a[20]+a[30]
+print(a[19:])
